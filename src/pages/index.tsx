0315a7f4738e16d976fb929d175b5105c36b2924
mimieee122 <giyomii030@naver.com>
2024-11-01 21:35:53 +0900

    final

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,18 +19,18 @@ export default function Home() {
 
     return (
         <div className="flex flex-row justify-center w-screen min-h-screen">
-            <div className="blue  text-black w-[1000px]  flex flex-col gap-[10px] justify-center items-center ">
+            <div className="blue  text-black w-full  flex flex-col gap-[10px] justify-center items-center ">
                 <div className="text-black login ">
                     {me.isSuccess ? (
-                        <div className="flex flex-col gap-[10px] contents-center justify-center">
-                            <div className="flex relative flex-row items-center gap-[10px] justify-between">
-                                <div className=" flex flex-row justify-center items-center  border-solid  h-[70px]  gap-[5px] w-56 text-black p-4 bg-[#ff3f6f]  rounded-xl">
-                                    <p className="yellow text-[40px] font-thin text-[white]">
+                        <div className="flex flex-col gap-[10px] contents-center  justify-center">
+                            <div className="flex  flex-row items-center gap-[0px] justify-center">
+                                <div className=" flex flex-row justify-center items-center  border-solid  h-[50px]  gap-[5px] text-black p-4 bg-[#ff3f6f]  rounded-xl">
+                                    <p className="yellow text-[30px] font-thin text-[white]">
                                         <span>HI, </span>
                                         <span>{me.data.data?.nickname}</span>
                                     </p>
                                 </div>
-                                <div className="relative w-[350px]  h-[400px] ">
+                                <div className="relative w-[450px]  h-[400px] ">
                                     <Image
                                         src="/assets/images/think.png"
                                         fill // 부모 요소에 가득 차게 함
@@ -41,13 +41,13 @@ export default function Home() {
                             </div>
 
                             <Link className="self-center" href="/category">
-                                <div className="flex flex-col shadow-[0_0_20px_white] transition-shadow rounded-xl  bg-white bg-opacity-15 border-[#ff517d] border-[5px] border-solid  gap-[5px] justify-center">
-                                    <button className="signIn font-thin text-[40px] text-[#ff8fab]">
+                                <div className="flex flex-col w-[580px] shadow-[0_0_20px_white] transition-shadow rounded-xl  bg-white bg-opacity-15 border-[#ff517d] border-[5px] border-solid  gap-[5px] justify-center">
+                                    <button className="signIn font-thin text-[30px] text-[#ff8fab]">
                                         IT&#39;S YOUR FLAN
                                     </button>
 
                                     <div className="flex flex-row justify-center items-center gap-[10px]">
-                                        <div className="self-center relative w-[300px]  h-[180px] mt-[2px] mb-[15px]">
+                                        <div className="self-center relative w-[200px]  h-[150px] mt-[2px] mb-[15px]">
                                             <Image
                                                 src="/assets/images/calendar_width.png"
                                                 fill // 부모 요소에 가득 차게 함
@@ -55,7 +55,7 @@ export default function Home() {
                                                 className="object-contain"
                                             />
                                         </div>
-                                        <div className="self-center relative w-[150px]  h-[150px] mt-[2px] mb-[15px]">
+                                        <div className="self-center relative w-[100px]  h-[100px] mt-[2px] mb-[15px]">
                                             <Image
                                                 src="/assets/images/cursor.png"
                                                 fill // 부모 요소에 가득 차게 함
