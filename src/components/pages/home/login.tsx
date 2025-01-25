faa5d6933a0012b06d19e581bbcfdfcc137d1d57
mimieee122 <giyomii030@naver.com>
2025-01-26 03:27:29 +0900

    zzz

--- src/components/pages/home/login.tsx
+++ src/components/pages/home/login.tsx
@@ -26,37 +26,38 @@ export function Login() {
     return (
         <form
             onSubmit={handleSubmit(onSubmitLogin)} // handleSubmit으로 폼 제출
-            className="flex flex-col all justify-center items-center text-center gap-[20px] text-black p-4"
+            className="flex flex-col all justify-center items-center text-center gap-[14px] text-black p-4"
         >
-            <div className="flex flex-row gap-[10px] items-center inLogo justify-center">
-                <div className=" relative flex flex-col emoji gap-0 items-center w-[300px] h-[250px]">
+            <div className="flex flex-col gap-[0px] items-center inLogo justify-center">
+                <div className=" relative flex flex-col emoji gap-0 items-center w-[400px] h-[252px]">
                     <Image
-                        src="/assets/images/flan emoji.png"
+                        src="/assets/images/final face.svg"
                         fill // 부모 요소에 가득 차게 함
                         alt="flan 이모지"
                         className="object-fill"
                     />
                 </div>
-                <div className="flex flex-col justify-center items-center">
-                    <div className=" relative flex flex-col justify-center  gap-0 items-center loGo w-[380px] h-[120px] mt-[15px] ">
-                        <Image
-                            src="/assets/images/flan.png"
-                            fill // 부모 요소에 가득 차게 함
-                            alt="flan 로고"
-                            className="object-fill logo"
-                        />
-                    </div>
-                    <div className="flex flex-row gap-[10px] mt-1 font-thin w-[400px] tell h-[5px] text-[18px] sub p-0 ">
+
+                <div className="relative gap-0 loGo w-[320px] h-[100px]  ">
+                    <Image
+                        src="/assets/images/flan.png"
+                        width={400} // 원하는 가로 길이
+                        height={100} // 원하는 세로 길이
+                        alt="flan 로고"
+                        className="object-cover"
+                    />
+
+                    {/* <div className="flex flex-row gap-[10px] mt-1 font-thin w-[400px] tell h-[5px] text-[18px] sub p-0 ">
                         <p className=" text-[black] font-thin">CREATE </p>
                         <p className="yellow text-[#ffbd43] font-thin">PLAN</p>
                         <p className="yellow text-[#ffbd43]">FAST</p>
                         <p className=" text-[black]"> WITH</p>
                         <p className="yellow text-[#ffbd43]">FLAN</p>
-                    </div>
+                    </div> */}
                 </div>
             </div>
             <div className="flex flex-col yellowBox text-black p-5 shadow-[0_0_15px_#ffbd43] transition-shadow border-[#ffbd43] border-[3px] border-solid justify-center items-center text-center gap-[15px] w-[817px]  bg-white bg-opacity-10 rounded-xl">
-                <p className="text-[40px] signIn text-[#ffbd43]">SIGN IN</p>
+                <p className="text-[30px] signIn text-[#ffbd43]">SIGN IN</p>
 
                 <label htmlFor="id">ID</label>
                 <input
