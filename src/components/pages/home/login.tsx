3b20bcf2383cb7ef2217c1ceedd175001276fcb5
mimieee122 <giyomii030@naver.com>
2025-01-26 20:03:02 +0900

    ~~

--- src/components/pages/home/login.tsx
+++ src/components/pages/home/login.tsx
@@ -29,13 +29,14 @@ export function Login() {
             className="flex flex-col h-screen overflow-y-hidden all justify-center items-center text-center  text-black "
         >
             <div className="flex flex-col gap-[0px] items-center inLogo justify-center">
-                <div
-                    className="relative flex flex-col emoji gap-0 items-end w-[300px] h-[200px] bg-cover bg-no-repeat"
-                    style={{
-                        backgroundImage: "url('/assets/images/final face.svg')",
-                    }}
-                >
-                    {/* 필요한 컨텐츠 추가 */}
+                <div className="relative w-[300px] h-[200px]">
+                    <Image
+                        src="/assets/images/final face.svg"
+                        alt="flan 이모지"
+                        layout="fill"
+                        objectFit="cover"
+                        unoptimized
+                    />
                 </div>
 
                 <div className="relative gap-0 loGo w-[300px]   ">
